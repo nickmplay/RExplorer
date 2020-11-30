617b291ddca2e513ed2b1c0c7df24362567c332f
Nick M <nickmontagni@hotmail.com>
2020-11-30 10:14:16 +0000

    fixed CORS issue

--- server/rexplorer_api.R
+++ server/rexplorer_api.R
@@ -1,6 +1,24 @@
 # rexplorer_api.R 
+
+#' @filter cors
+cors <- function(req, res) {
+  
+  res$setHeader("Access-Control-Allow-Origin", "*")
+  
+  if (req$REQUEST_METHOD == "OPTIONS") {
+    res$setHeader("Access-Control-Allow-Methods","*")
+    res$setHeader("Access-Control-Allow-Headers", req$HTTP_ACCESS_CONTROL_REQUEST_HEADERS)
+    res$status <- 200 
+    return(list())
+  } else {
+    plumber::forward()
+  }
+  
+}
+
 #* @get /dirR
 dirR <- function(path){
+  # run local query
   subd_list <- list.dirs(path, recursive = F, full.names = F)
   file_list <- setdiff(list.files(path), subd_list)
   list(dir = path, subDirs = subd_list, files = file_list)
@@ -9,4 +27,4 @@ dirR <- function(path){
 #* @assets ../src /
 list()
 
-# C:\Users\nickm\Documents\2_Work
+# http://127.0.0.1:8000/dirR?path=C:/Users/nickm/Documents/2_Work
