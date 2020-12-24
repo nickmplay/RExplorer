bfc48e79571955f3f2d7263c99c03253b130c323
Nick M <nickmontagni@hotmail.com>
2020-12-24 15:13:55 +0000

    added python server

---
+++ python/servePy.py
@@ -0,0 +1,11 @@
+# https://programminghistorian.org/en/lessons/creating-apis-with-python-and-flask
+import flask
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Hello World</h1>"
+
+app.run()
